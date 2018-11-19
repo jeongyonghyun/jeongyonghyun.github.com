52b2735f1e4c2ca93ad474788cd1d7495411ce96
jeongyonghyun <45089709+jeongyonghyun@users.noreply.github.com>
2018-11-19 13:42:28 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-// Generate random room name if needed
+const possibleEmojis = [
+  '🐀','🐁','🐭','🐹','🐂','🐃','🐄','🐮','🐅','🐆','🐯','🐇','🐐','🐑','🐏','🐴',
+  '🐎','🐱','🐈','🐰','🐓','🐔','🐤','🐣','🐥','🐦','🐧','🐘','🐩','🐕','🐷','🐖',
+  '🐗','🐫','🐪','🐶','🐺','🐻','🐨','🐼','🐵','🙈','🙉','🙊','🐒','🐉','🐲','🐊',
+  '🐍','🐢','🐸','🐋','🐳','🐬','🐙','🐟','🐠','🐡','🐚','🐌','🐛','🐜','🐝','🐞',
+];
+function randomEmoji() {
+  var randomIndex = Math.floor(Math.random() * possibleEmojis.length);
+  return possibleEmojis[randomIndex];
+}
+
+const emoji = randomEmoji();
+const name = prompt("What's your name?");
+
+// Generate random chat hash if needed
 if (!location.hash) {
   location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
-const roomHash = location.hash.substring(1);
+const chatHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone('63wnzap0klxFE9at');
-// Room name needs to be prefixed with 'observable-'
-const roomName = 'observable-' + roomHash;
+const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
+// Scaledrone room name needs to be prefixed with 'observable-'
+const roomName = 'observable-' + chatHash;
+// Scaledrone room used for signaling
+let room;
+
 const configuration = {
   iceServers: [{
-    urls: 'stun:stun.l.google.com:19302'
+    url: 'stun:stun.l.google.com:19302'
   }]
 };
-let room;
+// RTCPeerConnection
 let pc;
+// RTCDataChannel
+let dataChannel;
 
-
-function onSuccess() {};
-function onError(error) {
-  console.error(error);
-};
-
+// Wait for Scaledrone signalling server to connect
 drone.on('open', error => {
   if (error) {
     return console.error(error);
@@ -29,13 +43,16 @@ drone.on('open', error => {
   room = drone.subscribe(roomName);
   room.on('open', error => {
     if (error) {
-      onError(error);
+      return console.error(error);
     }
+    console.log('Connected to signaling server');
   });
   // We're connected to the room and received an array of 'members'
   // connected to the room (including us). Signaling server is ready.
   room.on('members', members => {
-    console.log('MEMBERS', members);
+    if (members.length >= 3) {
+      return alert('The room is full');
+    }
     // If we are the second user to connect to the room we will be creating the offer
     const isOfferer = members.length === 2;
     startWebRTC(isOfferer);
@@ -43,7 +60,7 @@ drone.on('open', error => {
 });
 
 // Send signaling data via Scaledrone
-function sendMessage(message) {
+function sendSignalingMessage(message) {
   drone.publish({
     room: roomName,
     message
@@ -51,63 +68,56 @@ function sendMessage(message) {
 }
 
 function startWebRTC(isOfferer) {
+  console.log('Starting WebRTC in as', isOfferer ? 'offerer' : 'waiter');
   pc = new RTCPeerConnection(configuration);
 
   // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
   // message to the other peer through the signaling server
   pc.onicecandidate = event => {
     if (event.candidate) {
-      sendMessage({'candidate': event.candidate});
+      sendSignalingMessage({'candidate': event.candidate});
     }
   };
 
-  // If user is offerer let the 'negotiationneeded' event create the offer
+
   if (isOfferer) {
+    // If user is offerer let them create a negotiation offer and set up the data channel
     pc.onnegotiationneeded = () => {
-      pc.createOffer().then(localDescCreated).catch(onError);
+      pc.createOffer(localDescCreated, error => console.error(error));
+    }
+    dataChannel = pc.createDataChannel('chat');
+    setupDataChannel();
+  } else {
+    // If user is not the offerer let wait for a data channel
+    pc.ondatachannel = event => {
+      dataChannel = event.channel;
+      setupDataChannel();
     }
   }
-  
-  if(member.id==[0]){
-    navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    }).then(stream => {
-      // Display your local video in #localVideo element
-      localVideo.srcObject = stream;
-      // Add your stream to be sent to the conneting peer
-      stream.getTracks().forEach(track => pc.addTrack(track, stream));
-    }, onError);
-  }else{
-      // When a remote stream arrives display it in the #remoteVideo element
-        pc.ontrack = event => {
-      const stream = event.streams[0];
-      if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
-        remoteVideo.srcObject = stream;
-    }
-  };
-  }
-  
+
+  startListentingToSignals();
+}
+
+function startListentingToSignals() {
   // Listen to signaling data from Scaledrone
   room.on('data', (message, client) => {
     // Message was sent by us
     if (client.id === drone.clientId) {
       return;
     }
-
     if (message.sdp) {
       // This is called after receiving an offer or answer from another peer
       pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+        console.log('pc.remoteDescription.type', pc.remoteDescription.type);
         // When receiving an offer lets answer it
         if (pc.remoteDescription.type === 'offer') {
-          pc.createAnswer().then(localDescCreated).catch(onError);
+          console.log('Answering offer');
+          pc.createAnswer(localDescCreated, error => console.error(error));
         }
-      }, onError);
+      }, error => console.error(error));
     } else if (message.candidate) {
       // Add the new ICE candidate to our connections remote description
-      pc.addIceCandidate(
-        new RTCIceCandidate(message.candidate), onSuccess, onError
-      );
+      pc.addIceCandidate(new RTCIceCandidate(message.candidate));
     }
   });
 }
@@ -115,7 +125,64 @@ function startWebRTC(isOfferer) {
 function localDescCreated(desc) {
   pc.setLocalDescription(
     desc,
-    () => sendMessage({'sdp': pc.localDescription}),
-    onError
+    () => sendSignalingMessage({'sdp': pc.localDescription}),
+    error => console.error(error)
   );
 }
+
+// Hook up data channel event handlers
+function setupDataChannel() {
+  checkDataChannelState();
+  dataChannel.onopen = checkDataChannelState;
+  dataChannel.onclose = checkDataChannelState;
+  dataChannel.onmessage = event =>
+    insertMessageToDOM(JSON.parse(event.data), false)
+}
+
+function checkDataChannelState() {
+  console.log('WebRTC channel state is:', dataChannel.readyState);
+  if (dataChannel.readyState === 'open') {
+    insertMessageToDOM({content: 'WebRTC data channel is now open'});
+  }
+}
+
+function insertMessageToDOM(options, isFromMe) {
+  const template = document.querySelector('template[data-template="message"]');
+  const nameEl = template.content.querySelector('.message__name');
+  if (options.emoji || options.name) {
+    nameEl.innerText = options.emoji + ' ' + options.name;
+  }
+  template.content.querySelector('.message__bubble').innerText = options.content;
+  const clone = document.importNode(template.content, true);
+  const messageEl = clone.querySelector('.message');
+  if (isFromMe) {
+    messageEl.classList.add('message--mine');
+  } else {
+    messageEl.classList.add('message--theirs');
+  }
+
+  const messagesEl = document.querySelector('.messages');
+  messagesEl.appendChild(clone);
+
+  // Scroll to bottom
+  messagesEl.scrollTop = messagesEl.scrollHeight - messagesEl.clientHeight;
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', () => {
+  const input = document.querySelector('input[type="text"]');
+  const value = input.value;
+  input.value = '';
+
+  const data = {
+    name,
+    content: value,
+    emoji,
+  };
+
+  dataChannel.send(JSON.stringify(data));
+
+  insertMessageToDOM(data, true);
+});
+
+insertMessageToDOM({content: 'Chat URL is ' + location.href});
