4ec3b32105eedcbc771efca166a07d2e9f598e3e
jeongyonghyun <45089709+jeongyonghyun@users.noreply.github.com>
2018-11-21 08:08:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -77,11 +77,13 @@ function startWebRTC(isOfferer) {
       remoteVideo.srcObject = stream;
     }
   };
-
-  navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  }).then(stream => {
+  
+　const prop = {
+     audio : true,
+     vidro : {width:1080px, height:640px}
+ };
+    
+  navigator.mediaDevices.getUserMedia(prop).then(stream => {
     // Display your local video in #localVideo element
     localVideo.srcObject = stream;
     // Add your stream to be sent to the conneting peer
