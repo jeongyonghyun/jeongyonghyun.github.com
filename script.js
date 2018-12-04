10c60992af7f4d4cdd5b787e35e9388d8fb572e0
jeongyonghyun <45089709+jeongyonghyun@users.noreply.github.com>
2018-12-04 12:06:01 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
 let mediaRecorder;
 let recordedBlobs;
 let sourceBuffer;
-const localVideo = document.querySelector('video#localVideo'); //
+const liveVideo = document.querySelector('video#localVideo'); //
 const recordedVideo = document.querySelector('video#recorded');
 const recordButton = document.querySelector('button#record');
 const playButton = document.querySelector('button#play');
@@ -181,7 +181,7 @@ recordButton.onclick = toggleRecording;
 playButton.onclick = play;
 downloadButton.onclick = download;
     
-const stream = localVideo.captureStream();
+let stream = liveVideo.captureStream();
 console.log("start stream capture from local video : ", stream);
     
 function handleSourceOpen(event) {
